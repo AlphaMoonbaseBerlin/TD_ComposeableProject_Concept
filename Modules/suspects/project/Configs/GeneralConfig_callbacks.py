b44bb883c4e827f1a6c2be69f6cef85651018a3d
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-10-23 22:34:23 +0200

    Conceptual Setup using dynamic configJson files based on plugins.

--- Modules/suspects/project/Configs/GeneralConfig_callbacks.py
+++ Modules/suspects/project/Configs/GeneralConfig_callbacks.py
@@ -2,39 +2,26 @@
 Name : GeneralConfig_callbacks
 Author : Wieland@AMB-ZEPH15
 Saveorigin : Project.toe
-Saveversion : 2022.35320
+Saveversion : 2023.11880
 Info Header End'''
 
 def GetConfigSchema(configModule:"SchemaObjects", configComp:"JsonConfig") -> dict:
-	positiveValue = configModule.ConfigValue(default = 4, validator = lambda value : value > 0)
+	
 	
 	return {
-		"NamedList" : configModule.NamedList(),
-    	"Some_Integer" : configModule.ConfigValue( 
-			default = -2, 
-			validator = lambda value: value < 4,
-			typecheck = (float, int)
-		 ),
-    	"Some_String"   : configModule.ConfigValue( default = "foo", validator= lambda value: isinstance( value, str)),
-    	"Subdata" : configModule.CollectionDict({
-        	"Sub" : configModule.ConfigValue( default = 13, validator= lambda value: value < 14 ),
-			"SubDict" : configModule.CollectionDict({
-				"SubSubValue" : configModule.ConfigValue( 
-					default = "Foobar",
-					typecheck = str,
-					parser = lambda value: value.upper()
-				)
-			})
-    		}),
-    	"Sublist" : configModule.CollectionList( ),
-	    "Prefilled_Sublist" : configModule.CollectionList(
-			["Hello", "WOrld"]
-		),
-		"PositiveValue" : positiveValue(),
-		"Prefilled_Validated" : configModule.CollectionList(
-			items = [1,2,3],
-			default_member = configModule.ConfigValue(default = -1, validator = lambda value: value < 3)
-		)
+		"Global" : configModule.CollectionDict({
+			"TransportPort" : configModule.ConfigValue(
+				9500, comment ="The port that is used for the transport using TouchIn/Out"
+			)
+		}),
+		"Plugins" : configModule.CollectionDict({
+			operator.name : configModule.CollectionDict({
+				"Active" : configModule.ConfigValue( False ),
+				"Settings" : configModule.CollectionDict({
+					parameter.name : configModule.ConfigValue( parameter.eval() ) for parameter in operator.customPars if parameter.page.name == "Settings"
+				})
+			}) for operator in iop.Store.Pluginmanager.Plugins.values()
+		})
 		}
 		
 		
